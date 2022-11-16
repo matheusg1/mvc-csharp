a65855b80794c3b13ad30c1758dd7c4f71af2ee4
matheus.silva <tecmatheusg@hotmail.com>
2022-11-16 13:14:13 -0300

    Usando dictionary, responses e limitando paths

--- Alura.ListaLeitura.App/Startup.cs
+++ Alura.ListaLeitura.App/Startup.cs
@@ -1,6 +1,9 @@
 ﻿using Alura.ListaLeitura.App.Repositorio;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace Alura.ListaLeitura.App
@@ -9,16 +12,38 @@ namespace Alura.ListaLeitura.App
     {
         public void Configure(IApplicationBuilder app)
         {
-            app.Run(LivrosParaLer);
+            app.Run(Roteamento);
         }
 
-        public Task LivrosParaLer(HttpContext context)
+        public Task Roteamento(HttpContext context)
         {
-            HttpContext contexto;
-
             var _repo = new LivroRepositorioCSV();
-            return context.Response.WriteAsync(_repo.ParaLer.ToString());
-            
+
+            var path = context.Request.Path;
+            var caminhosAtendidos = new Dictionary<string, string>
+            {
+                { "/Livros/ParaLer", _repo.ParaLer.ToString() },
+                { "/Livros/Lendo", _repo.Lendo.ToString() },
+                { "/Livros/Lidos", _repo.Lidos.ToString() }
+            };
+
+            if (caminhosAtendidos.ContainsKey(context.Request.Path))
+            {
+                return context.Response.WriteAsync(caminhosAtendidos[context.Request.Path]);
+            }
+            else
+            {
+                return context.Response.WriteAsync("Caminho inexistente");
+            }
         }
+
+    public Task LivrosParaLer(HttpContext context)
+    {
+        HttpContext contexto;
+
+        var _repo = new LivroRepositorioCSV();
+        return context.Response.WriteAsync(_repo.ParaLer.ToString());
+
     }
+}
 }
